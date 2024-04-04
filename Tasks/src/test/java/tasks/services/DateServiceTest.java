c29ab5ab9f9476bd51665d26911d71ee4f87c2d7
StefanaHanc <stefanahanc@yahoo.com>
2024-04-04 16:42:38 +0300

    Add ECP

--- Tasks/src/test/java/tasks/services/DateServiceTest.java
+++ Tasks/src/test/java/tasks/services/DateServiceTest.java
@@ -52,6 +52,45 @@ class DateServiceTest {
         assertEquals(localDate.getHour(), 23);
         assertEquals(localDate.getMinute(), 59);
     }
+    @Test
+    void testGetDateMergedWithTime1ECP() throws ParseException {
+
+        // Ora și data sunt ambele valide
+        String time = "12:30";
+        Date noTimeDate = formatter.parse("2023-04-15");
+        // act
+        Date date = dateService.getDateMergedWithTime(time, noTimeDate);
+        LocalDateTime localDate = toLocalDate(date);
+        // assert
+        assertEquals(localDate.getYear(), 2023);
+        assertEquals(localDate.getMonth(), Month.APRIL);
+        assertEquals(localDate.getDayOfMonth(), 15);
+        assertEquals(localDate.getHour(), 12);
+        assertEquals(localDate.getMinute(), 30);
+    }
+    @Test
+    void testGetDateMergedWithTime2ECP() throws ParseException {
+      //  Ora valida dar data invalida ziua >31
+        /// arrange
+        String time = "22:00";
+        Date noTimeDate = formatter.parse("2023-05-32");
+        // act && assert
+        assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(time, noTimeDate));
+    }
+
+    @Test
+    void testGetDateMergedWithTime3ECP() throws ParseException {
+        // Ora invalidă, dar data validă ora>24:00
+        /// arrange
+        String time = "24:01";
+        Date noTimeDate = formatter.parse("2023-05-32");
+
+        // act && assert
+        assertThrows(IllegalArgumentException.class, () -> dateService.getDateMergedWithTime(time, noTimeDate));
+    }
+
+
+
 
     @Test
     void testGetDateMergedWithTime2() throws ParseException {
